2318349d6a837d8f44c49cfc259c1f8846c9d43f
Stefan I. Strogin <s.strogin@partner.samsung.com>
2014-12-05 22:53:48 +0300

    stacktrace: add seq_print_stack_trace()
    Add a function seq_print_stack_trace() which prints stacktraces to seq_files.
    
    Signed-off-by: Stefan I. Strogin <s.strogin@partner.samsung.com>

--- kernel/stacktrace.c
+++ kernel/stacktrace.c
@@ -57,6 +57,23 @@ int snprint_stack_trace(char *buf, size_t size,
 }
 EXPORT_SYMBOL_GPL(snprint_stack_trace);
 
+void seq_print_stack_trace(struct seq_file *m, struct stack_trace *trace,
+			int spaces)
+{
+	int i;
+
+	if (WARN_ON(!trace->entries))
+		return;
+
+	for (i = 0; i < trace->nr_entries; i++) {
+		unsigned long ip = trace->entries[i];
+
+		seq_printf(m, "%*c[<%p>] %pS\n", 1 + spaces, ' ',
+				(void *) ip, (void *) ip);
+	}
+}
+EXPORT_SYMBOL_GPL(seq_print_stack_trace);
+
 /*
  * Architectures that do not implement save_stack_trace_tsk or
  * save_stack_trace_regs get this weak alias and a once-per-bootup warning
